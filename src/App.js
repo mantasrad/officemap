477ba26912acdaf743d2342a157d2a7d71383f27
mradzvilavicius <mantas.radzvilavicius@bazaarvoice.com>
2019-03-30 06:35:57 -0500

    v2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          Office Map
+          Office Map v2
         </header>
       </div>
     );
